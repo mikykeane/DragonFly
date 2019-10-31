0a57a5007e9e2cf73be3db9fb8dfcaaba8c0222c
maria <maria@172.20.49.194>
2019-10-31 14:45:45 +0100

    Creada función que nos indica hacia donde debemos movernos

--- src/dragonfly/Gonio.java
+++ src/dragonfly/Gonio.java
@@ -6,12 +6,11 @@
 package dragonfly;
 
 import com.eclipsesource.json.JsonObject;
-import java.util.ArrayList;
 import java.util.HashMap;
 /**
  *
  * Clase que gestiona la distancia y la dirección a la que se encuentra el objetivo
- * @author miguelkeane
+ * @author miguelkeane, María del Mar García Cabello
  */
 public class Gonio {
     //Distancia a la casilla objetivo en numero de celdas
@@ -19,53 +18,58 @@ public class Gonio {
     //Posicion de la casilla objetivo en angulos
     double angle; 
     //Aqui guardaremos los datos de los angulos
-    HashMap<String, Integer> angulos; 
+    HashMap<Integer, String> angulos; 
  
         /**
      * @author María del Mar García Cabello
      * @param object
+     * @return movimiento
      */
-    public void GonioParser(JsonObject object){
+    public String GonioParser(JsonObject object){
            angulos=new HashMap<>();
             //Rellenamos el vector con los grados de los angulos
-           angulos.put("Norte",0);
-           angulos.put("NorEste",45);
-           angulos.put("Este",90);
-           angulos.put("SurEste",135);
-           angulos.put("Sur",180);
-           angulos.put("SurOeste",225);
-           angulos.put("Este",270);
-           angulos.put("NorOeste",315);
+           angulos.put(0,"Norte");
+           angulos.put(45,"NorEste");
+           angulos.put(90,"Este");
+           angulos.put(135,"SurEste");
+           angulos.put(180,"Sur");
+           angulos.put(225,"SurOeste");
+           angulos.put(270,"Este");
+           angulos.put(315,"NorOeste");
         
         //Obtenemos la distancia en numero de celdas al objetivo 
         distance= object.get("distance").asObject().asInt();
         //Obtenemos la dirección al que está el objetivo(en angulos)
         angle = object.get("angle").asObject().asDouble();
         
-        int minimo = 360;
-        int aux= Math.abs(angle - /*Aqui van cada una de las direcciones, N,S,E,W,NE,NW,SE,SW*/) % 360;
-        int distancia;
-        if(aux>180)
-            distancia=360-aux;
-        else
-            distancia=aux;
+        
+        double anguloActual;
+        //Distancia que hay entre el objetivo y cada uno de los angulos de movimiento posibles
+        double distancia;
+        //Distancia minima y hacia donde nos deberemos mover
+        double distanciaMinima=360;
+        String movimiento="Norte";//Por defecto nos moveremos al norte
         
         //Vamos a comparar la dirección del objetivo con las direcciones a las que nos podemos mover
         //para encontrar la más cercana
-        for(int i=0; i<8;i++){
+        
+        //Recorremos el hasMap
+        for(int i : angulos.keySet()) {
+            //vemos que angulo nos conviene mas
+            anguloActual = Math.abs(angle-i)%360; 
+            if(anguloActual>180){
+                distancia=360-anguloActual;
+            }else distancia=anguloActual;
             
-        }
+            //Nos quedamos con la distancia mas pequeña
+            if(distanciaMinima>distancia) {
+                distanciaMinima=distancia;
+                movimiento=angulos.get(i);
+            }
+                
+        } 
         
-        
-       /* 
-          public static int distance(int alpha, int beta) {
-        int phi = Math.abs(beta - alpha) % 360;       // This is either the distance or 360 - distance
-        int distance = phi > 180 ? 360 - phi : phi;
-        return distance;
-        
-        */
-        
+        return movimiento;
     }
-        
-    }
+       
 }
