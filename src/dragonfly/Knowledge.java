8a4de23e21ef85e69b2c31a9f37dbd12bea3ff08
maria <maria@DESKTOP-9BRMSGM.home>
2019-11-10 13:47:34 +0100

    Limpieza de codigo y documentacion

--- src/dragonfly/Knowledge.java
+++ src/dragonfly/Knowledge.java
@@ -11,20 +11,22 @@ package dragonfly;
  */
 public class Knowledge {
     String mapID; 
-    
-    
-    // Cómo vamos a guardar los resultados? Es de las decisiones más importantes. Sabéis CSV?? Así podemos guardar los resultados de los mapas
-    // hacer exploraciones, ir guardando el mapa y lo que hacemos es poner al agente en 2 modos. Primero en exploración, mapeamos todo lo posible
-    // Y guardamos los resultados de una iteración a otra. Luego otro con un algoritmo A* o lo que sea que llegue directo al destino, haciendo uso 
-    // todo el conocimiento que tenga guardado. 
 
-    
+    /**
+     * @author Miguel Keane
+     * @param myMap
+     */    
     Knowledge(String myMap) {
         mapID = myMap;
         // Load(mapID)  o algo similar sería lo suyo
         
     }
-
+    
+    
+    /**
+     * @author Miguel Keane
+     * 
+     */
     Knowledge() {
         mapID="";
     }
