cc1fc92d2e501f2088227295bd54ed4c7bf01af4
maria <maria@DESKTOP-9BRMSGM.home>
2019-11-10 13:46:35 +0100

    Limpieza de codigo y documentacion

--- src/dragonfly/Fuel.java
+++ src/dragonfly/Fuel.java
@@ -21,22 +21,20 @@ public class Fuel {
     public void FuelParser(JsonObject object)
     {
         //Parseamos los datos de JSON
-       //String aux;
         //Obtenemos la cantidad de fuel que nos queda
         fuel= object.get("perceptions").asObject().get("fuel").asDouble();
-        //fuel = Double.parseDouble(aux);
     }
     
      /**
      *  @author María del Mar García Cabello
-     *
+     *  Hacemos recarga del fuel
      */
     public void refuel(){
         fuel=100;
     }
     /**
      *  @author María del Mar García Cabello
-     *
+     *  Gastamos 0.5% de fuel 
      */
     public void useFuel(){
         fuel=fuel-0.5;
